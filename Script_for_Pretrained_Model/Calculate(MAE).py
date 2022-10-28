e3529b5eb4317318327392b3e45a8e6796fb20ca
kable <kablely21@gmail.com>
2022-10-28 14:22:55 +0800

    update

---
+++ Script_for_Pretrained_Model/Calculate(MAE).py
@@ -0,0 +1,165 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+import torch
+from PIL import Image
+import numpy as np
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+import shutil
+import time
+import cv2
+from scipy.stats import pearsonr
+from sklearn import metrics
+
+def print_log(print_string, log_file, visible=True):
+    if visible:
+        print("{}".format(print_string))
+    # write in log file
+    log_file.write('{}\n'.format(print_string))
+    # log the data into the file
+    log_file.flush()
+
+size = (256, 256)
+# set device as cpu or GPU
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+# device = torch.device("cpu")
+
+
+
+
+# ---calculate size of parameters---
+# from fvcore.nn import FlopCountAnalysis, parameter_count_table
+# Parameters = parameter_count_table(GAN_generator)
+# print('Parameters:', Parameters)
+
+#
+# def print_model_parm_nums(model):
+#     total = sum([param.nelement() for param in model.parameters()])
+#     print('  + Number of params: %.2fM' % (total / 1e6))
+#
+# Parameters_sum=print_model_parm_nums(model.netG)
+
+
+def get_transform():
+    transform_list = []
+    transform_list += [transforms.ToTensor()]
+    transform_list += [transforms.Normalize((0.5, 0.5, 0.5),(0.5, 0.5, 0.5))]
+    return transforms.Compose(transform_list)
+# preprocessing
+G_transform = get_transform()
+
+def picture_strengthen(GAN_generator, file_path,device):
+    # Read images
+    test_img = cv2.imdecode(np.fromfile(file_path, dtype=np.uint8), 1)[:, :, ::-1]
+    # Copy original data
+    test_img_ori = test_img.copy()
+    # convert grey scale to RGB
+    test_img = Image.fromarray(test_img.astype('uint8')).convert('RGB')
+    # resize the image
+    img = test_img.resize(size)
+    # preprocessing
+    img = G_transform(img)
+    # remove 0 chanel
+    img = img.unsqueeze(0).to(device)
+    # forward propagation
+    if '-' in file_path:
+        file_path=file_path.replace('-','')
+    A,B,VR=os.path.split(file_path)[-1][:-4].split('_')[:3]
+    AP= float(A)/float(B)
+    volume = float(VR)
+    generated = GAN_generator.forward(img)
+    # get the matrix of image
+    image_numpy = generated.data[0].cpu().float().numpy()
+    # transpose chanel
+    image_numpy=np.transpose(image_numpy, (1, 2, 0))
+    # resolution +1，divided by 2.0，then time 255
+    image_numpy = (image_numpy+ 1) / 2.0 * 255.0
+    # limit the range of values in 0-255
+    image_numpy = np.clip(image_numpy, 0, 255)
+    # change the type of data to uint8
+    image_numpy = image_numpy.astype(np.uint8)
+    #
+    image_numpy=cv2.resize(image_numpy,(test_img_ori.shape[1],test_img_ori.shape[0]))
+
+    # save results
+    # format=os.path.splitext(file_path)[-1]
+    # # save_file_path = file_path.replace(format, "_strengthen" + format)
+    # save_file_path = os.path.join(save_dir,os.path.split(file_path)[-1])
+    # cv2.imencode(format, image_numpy[:,:,::-1])[1].tofile(save_file_path)
+    return image_numpy
+    # ------------------------------save end----------------------------------
+    # ------------------------------for test ---------------------------------
+    # format = os.path.splitext(file_path)[-1]
+    # save_file_path = os.path.join(save_dir, os.path.split(file_path)[-1])
+    # cv2.imencode(format, final_img[:, :, ::-1])[1].tofile(save_file_path)
+    # ------------------------------save end----------------------------------
+
+if __name__=='__main__':
+    src_dir=r'D:\GAN\pytorch-CycleGAN-and-pix2pix\datasets\Change of AR\Test\Change of AR_TO_test'
+    target_dir=r'D:\GAN\pytorch-CycleGAN-and-pix2pix\datasets\Change of AR\Test\Change of AR_AM_test'
+    model_dir=r'checkpoints/GAN_Control_Ensemble_pix2pix_aligned_Unet_jit'
+    #save_dir=src_dir+'_pix2pix_Ours_predicts_catt_{}'.format(os.path.split(model_path)[-1][:4])
+    log=open(os.path.join(model_dir,'log_MAE.txt'),'w',encoding='UTF-8')
+    print_log('model_dir:{}'.format(model_dir), log)
+    # if not os.path.exists(save_dir):
+    #     os.makedirs(save_dir)
+    file_list=os.listdir(src_dir)
+    MAE_all=[]
+    RMSE_all=[]
+    pearson_all=[]
+    # load model
+    model_list=os.listdir(model_dir)
+    if 'GAN_Control_Ensemble_pix2pix_aligned_Resnet_catt_256_jit.zip' in model_list:
+        model_list.remove('GAN_Control_Ensemble_pix2pix_aligned_Resnet_catt_256_jit.zip')
+    if 'latest_net_G_3_256_256_jit.pt' in model_list:
+        model_list.remove('latest_net_G_3_256_256_jit.pt')
+    if 'log_MAE.txt' in model_list:
+        model_list.remove('log_MAE.txt')
+
+    model_list.sort(key=lambda x:int(x.split('_')[0]))
+
+    for model_file in model_list:
+        model_path = os.path.join(model_dir,model_file)
+        # model_path=r'checkpoints/GAN_Control_Ensemble_pix2pix_aligned_Resnet256/400_net_G_params.pth
+        if '_jit.pt' in model_path:
+            GAN_generator = torch.jit.load(model_path, map_location='cpu').to(device)
+        else:
+            GAN_generator = torch.load(model_path, map_location='cpu').to(device)
+        GAN_generator.eval()
+        # print('model:', GAN_generator)
+        with torch.no_grad():
+            for idx,file in enumerate(file_list):
+                print('{}/{}'.format(idx, len(file_list)))
+                # predict results
+                file_path=os.path.join(src_dir,file)
+                predict_img=picture_strengthen(GAN_generator,file_path,device)
+                # GT
+                target=cv2.imread(os.path.join(target_dir,file))
+                # print('predict_img:{},target:{}'.format(predict_img.shape,target.shape))
+
+                y_true=target[:,:,0].reshape(-1)/255.0
+                y_true[y_true>0.5]=1.0
+                y_true[y_true<=0.5]=0
+                y_pred=predict_img[:,:,0].reshape(-1)/255.0
+                y_pred[y_pred>0.5]=1.0
+                y_pred[y_pred<=0.5]=0.0
+
+                # MAE，RMSE，Pearson
+                MAE = np.mean(np.abs(y_true - y_pred))
+                MAE_all.append(MAE)
+                MAE_2 = metrics.mean_absolute_error(y_true, y_pred)
+
+                RMSE = np.sqrt(np.mean(np.square(y_true - y_pred)))
+                RMSE_all.append(RMSE)
+
+                RMSE_2 = np.sqrt(metrics.mean_squared_error(y_true, y_pred))
+                pearson, ttest = pearsonr(y_true, y_pred)
+                pearson_all.append(pearson)
+
+        MAE_mean = np.mean(np.array(MAE_all))
+        RMSE_mean = np.mean(np.array(RMSE_all))
+        pearson_mean= np.mean(np.array(pearson_all))
+        #  print values
+        print_log('Epoch:{},MAE:{:.5f}, RMSE:{:.5f}, pearson:{:.5f}'.format(model_file.split('_')[0],MAE_mean,RMSE_mean , pearson_mean),log)
+    print('save_dir:',model_dir)
+    log.close()
---
+++ Script_for_Pretrained_Model/MAE_Curve_Combine.py
@@ -0,0 +1,131 @@
+#! -*- coding:utf-8 -*-
+import os
+import numpy as np
+'''
+Plot MAE，RMSE，Pearson
+
+'''
+
+# ------------------------------------ plot----------------------------
+import matplotlib.pyplot as plt
+
+# load results
+# Epoch:5,MAE:0.11857, RMSE:0.34286, pearson:0.64998
+log_path = r"C:\Users\KABLE21\Desktop\Shared_Results\results(MAE)\Baseline_log_MAE.txt"
+
+f=open(log_path,'r',encoding='UTF-8')
+# -------------- '(epoch: 1, iters: 100, time: 0.015, data: 5.072) G_GAN: 1.005 G_L1: 21.786 D_real: 0.533 D_fake: 0.728' -------
+MAE_all=[]
+RMSE_all=[]
+pearson_all=[]
+Epoch_all=[]
+bf_epoch=0
+
+for line in f.readlines():
+    line = line.strip()
+    if 'Epoch:' in line:
+        # Epoch:5,MAE:0.11857, RMSE:0.34286, pearson:0.64998
+        Epoch, MAE, RMSE, pearson =line.split(',')
+        Epoch=int(Epoch.split(':')[-1])
+        if Epoch < 400 and Epoch!=bf_epoch:
+            MAE=float(MAE[4:])
+            RMSE=float(RMSE[6:])
+            pearson=float(pearson[9:])
+
+            MAE_all.append(MAE)
+            RMSE_all.append(RMSE)
+            pearson_all.append(pearson)
+            Epoch_all.append(Epoch)
+
+            bf_epoch=Epoch
+            print(line)
+        if Epoch > 400:
+            break
+
+Baseline_MAE_all=MAE_all.copy()
+
+
+log_path = r"C:\Users\KABLE21\Desktop\Shared_Results\results(MAE)\FC_log_MAE.txt"
+f=open(log_path,'r',encoding='UTF-8')
+# -------------- '(epoch: 1, iters: 100, time: 0.015, data: 5.072) G_GAN: 1.005 G_L1: 21.786 D_real: 0.533 D_fake: 0.728' -------
+MAE_all_2=[]
+RMSE_all=[]
+pearson_all=[]
+Epoch_all=[]
+bf_epoch=0
+
+for line in f.readlines():
+    line = line.strip()
+    if 'Epoch:' in line:
+        # Epoch:5,MAE:0.11857, RMSE:0.34286, pearson:0.64998
+        Epoch, MAE, RMSE, pearson =line.split(',')
+        Epoch=int(Epoch.split(':')[-1])
+        if Epoch < 400 and Epoch!=bf_epoch:
+            MAE=float(MAE[4:])
+            RMSE=float(RMSE[6:])
+            pearson=float(pearson[9:])
+
+            MAE_all_2.append(MAE)
+            RMSE_all.append(RMSE)
+            pearson_all.append(pearson)
+            Epoch_all.append(Epoch)
+
+            bf_epoch=Epoch
+            print(line)
+        if Epoch > 400:
+            break
+
+
+results_list=[Baseline_MAE_all,MAE_all_2]
+# save directory
+save_dir='results'
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+# creat a figure，set the aspect ratio of figures and dpi
+fig1 = plt.figure(figsize=(8, 5), dpi=200)
+ax = plt.subplot(111)
+
+"""
+# in 'bo-', b is blue, o is O marker, - is solid line and so on
+# color：r-red  g-green    b-blue  y-yellow  w-white  k-black  c-cyan  m-magenta
+"""
+
+markersize = 5
+linewidth = 1.0
+# with mark
+line_style_list=['bo-','gv--','ms-.','ch:','rD-']
+# without mark
+line_style_list = ['b-', 'r-', 'c:', 'r-']
+label_list=['Baseline','pix2pix with feature controller']
+y_all=[]
+
+# x=np.linspace(0,len(G_GAN_all)-1, len(G_GAN_all))
+for idx,result in enumerate(results_list):
+    y = results_list[idx]
+    #assert len(x)==len(y)
+    ax.plot(Epoch_all, y, line_style_list[idx], label=label_list[idx], markersize=markersize, linewidth=linewidth)
+
+
+#para1：loc，repsent location
+# best  upper right upper left    lower left  lower right right   center left center right     lower center
+# upper center    center
+
+# set legend property
+plt.legend(loc=0, ncol=2, fontsize=13)
+# set the label of x axis
+plt.xlabel('Epochs', fontsize=15)
+# set the label of y axis
+plt.ylabel('MAE', fontsize=15)
+
+# set the limitation of x
+# plt.xlim([0, 300])
+# set the limitation of y
+plt.ylim([0.07, 0.14])
+
+# save figure
+plt.savefig(os.path.join(save_dir, log_path.split('/')[-1][:-4]+'_MAE_Ensemble.png'), dpi=200, bbox_inches='tight')
+# show the figure
+plt.show()
+# close figure
+plt.close()
