0201fb5cb7d51bc8b2f6b78358a24f196044f86d
kable <kablely21@gmail.com>
2022-11-15 21:54:09 +0800

    update

--- Script_for_Pretrained_Model/info_net.py
+++
@@ -1,5 +0,0 @@
-from torchinfo import summary
-model_path=r"F:\pix2pix_FC\checkpoints\pix2pix_FC\295_net_G.pth"
-model = GAN_generator = torch.load(model_path, map_location='cpu').to('cpu')
-batch_size = 1
-summary(model, input_size=(batch_size, 1, 28, 28))
